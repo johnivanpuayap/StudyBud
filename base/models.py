635004d8431a1c89c646b10ed40418a2f1cc8c45
John Ivan Puayap <johnivanpuayap@gmail.com>
2023-09-18 16:22:00 +0800

    Change database to MySQL and add Models

--- base/models.py
+++ base/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Room(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
+
+    # CharField should be used when you have a relatively short piece of text, like a name, title, or code,
+    # and you want to limit the maximum length of the text.
+    name = models.CharField(max_length=200)
+
+    # TextField should be used when you expect to store a potentially large amount of text,
+    # such as paragraphs of text, descriptions, or comments.
+    description = models.TextField(null=True, blank=True)
+
+    # participants =
+
+    # auto now takes a snapshot on every time we save this item
+    updated = models.DateTimeField(auto_now=True)
+
+    # auto now add only takes a timestamp when we first save or create this instance
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.body[0:50]
--- base/views.py
+++ base/views.py
@@ -1,26 +1,23 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import Room
 # Create your views here.
 
-rooms = [
-    {'id': 1, 'name': "Let's Learn Python!"},
-    {'id': 2, 'name': "Design with me!"},
-    {'id': 3, 'name': "Backend developers!"},
-]
+# rooms = [
+#     {'id': 1, 'name': "Let's Learn Python!"},
+#     {'id': 2, 'name': "Design with me!"},
+#     {'id': 3, 'name': "Backend developers!"},
+# ]
 
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
 
 def room(request, room_id):
     # return HttpResponse('Room')
-    room = None
-    for i in rooms:
-        if i['id'] == int(room_id):
-            room = i
-
+    room = Room.objects.get(id=room_id)
     context = {'room': room}
     return render(request, 'base/room.html', context)
