bc35f0d67581def39ad70064a44a031ba75876db
John Ivan Puayap <johnivanpuayap@gmail.com>
2023-09-27 08:27:17 +0800

    Apply filter on the recent activity when selecting a topic
    Apply filter on the recent activity when selecting a topic
    
    Apply filter on the recent activity when selecting a topic

--- base/views.py
+++ base/views.py
@@ -75,7 +75,7 @@ def home(request):
     )
     topics = Topic.objects.all()
     room_count = rooms.count()
-    room_activities = Message.objects.all()
+    room_activities = Message.objects.filter(Q(room__topic__name__icontains=q))
 
     context = {
         'rooms': rooms,
