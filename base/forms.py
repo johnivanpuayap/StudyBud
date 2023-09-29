96cbcd1fcb46e27bd9b47832b467427fcded6413
John Ivan Puayap <johnivanpuayap@gmail.com>
2023-09-29 19:04:29 +0800

    Configure static files and add CSS

--- base/forms.py
+++ base/forms.py
@@ -1,9 +1,8 @@
 from django.forms import ModelForm
 from .models import Room
 
+
 class RoomForm(ModelForm):
     class Meta:
         model = Room
-        # Use all the fields
-        # fields = '__all__'
         fields = ['name', 'description', 'topic']
